700c4542740c673d914ff740baef75412f4825fe
Vlad Zelinsky <zelinskyvlad911@gmail.com>
2025-09-09 17:30:43 +0200

    Fix 1.0

--- services/auth-service/src/users/users.service.ts
+++ services/auth-service/src/users/users.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { UserModel, UserDoc } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument, UserDoc } from './user.schema';
+
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   findByEmail(email: string) {
-    return UserModel.findOne({ email }).lean<UserDoc>();
+    return this.userModel.findOne({ email }).lean<UserDoc>();
   }
+
   async createUser(email: string, passwordHash: string, name?: string) {
-    const doc = await UserModel.create({
+    const doc = await this.userModel.create({
       email,
       passwordHash,
       name,
       roles: ['user'],
     });
-    return doc.toObject() as UserDoc;
+    return doc.toObject() as unknown as UserDoc;
   }
 }
